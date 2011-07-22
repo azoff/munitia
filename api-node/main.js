48ba39335f06a9a476e6694efb991afa72c9694f
Jonathan Azoff <jon@azoffdesign.com>
2011-07-22 03:39:00 -0700

    Added DB connection and RESTful delegates

--- api-node/main.js
+++ api-node/main.js
@@ -1,12 +1,13 @@
 /*global require:true, console:true, process:true */
 require.paths.unshift(process.mainModule[0]);
 
-require('mongodb');
-
 if (process.argv.length > 2) {
 	require('config').load(process.argv[2], 'utf-8', function(config){
-		require('apiserver').start(config, 8080);
+		require('database').connect(config, function(database){			
+			require('apiserver').start(config, database);
+		});
 	});
 } else {
 	console.error('usage: %s %s path/to/environment.json', process.argv[0], process.argv[1]);
+	process.exit(1);
 }
