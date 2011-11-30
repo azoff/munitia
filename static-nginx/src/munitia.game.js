85ae4aa75010809022020b111661ab1f97a89c8e
Tracy Scott <tracy.scott@gmail.com>
2011-11-30 00:19:58 -0800

    form for entering new question. disabled time quantams in add_to_round

--- static-nginx/src/munitia.game.js
+++ static-nginx/src/munitia.game.js
@@ -79,18 +79,61 @@
             controller.hideLoader();
         },
 
-        loadQuestions: function(page) { var
-            coords = session.user.getCoords(),
-            args = { lt: coords.latitude, lg: coords.longitude };
+	loadQuestions: function(page, args) { 
+	    var coords = session.user.getCoords();
+            var findArgs = { lt: coords.latitude, lg: coords.longitude };
             controller.showLoader('loading questions');
-            namespace.api.get('find_questions_near', args).success(function(model){ 
-                module.renderQuestions(page, model); 
-            });
-        },
+	    var questions = page.data('questions');
+	    if ("cmd" in args) {
+		if (args.cmd == 'new_question') {
+		    module.renderNewQuestion(page);
+		    return;
+		} else if (args.cmd == 'view_question') {
+		    // TODO(tracy): IMPLEMENT THIS.
+		    module.renderSingleQuestion(page, args);
+		    return;
+		}
+	    }
+	    if (!questions) {
+		namespace.api.get('find_questions_near', findArgs).success(function(model){ 
+			module.renderQuestions(page, model); 
+	       });
+	    }
+	},
+	
+	renderNewQuestion: function(page) {
+	    var coords = session.user.getCoords();
+            var geoArgs = { lt: coords.latitude, lg: coords.longitude };
+	    controller.render('new_question', geoArgs, function(html) {
+		    if (html) {
+			page.find('.header').html('New Question');
+			page.find('.refresh').removeClass('ui-btn-active');
+			page.find('.content').empty().append(html); 
+			html.filter('ul').listview();
+			var new_question_form = $('#new_question_form');
+			new_question_form.submit(function(event) {
+				var form_data = new_question_form.serialize();
+				$.post('/create_new_question', form_data, function(response) {
+					if ("status" in response) {
+					    if (response.status == 200) {
+						page.data('questions', ''); // clear old questions
+						module.loadQuestions();
+					    } else {
+						renderError('Server error: ' + JSON.stringify(response));
+					    }
+					} else {
+					    renderError('Malformed server response: ' + JSON.stringify(response));
+					}
+				    });
+			    });
+			controller.hideLoader();
+		    } else { renderError('Unable to render new_question form'); }
+		});
+	},
 
-        renderQuestions: function(page, model) {
-            var questionObjs = model.data;
-            questionObjs.push({question: "Add question", answers: []});
+	renderQuestions: function(page, model) {
+	    questionObjs = model.data;
+	    questionObjs.push({question: 'Add question', answers: []});
             model = { questions: questionObjs };
             controller.render('question', model, function(html){
                 if (html) {
