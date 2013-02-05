a1c7817c56fc5d934629da0296246bcd94d3bc95
Jonathan Azoff <jon@azoffdesign.com>
2013-02-04 22:40:06 -0800

    Enable the download portion

--- tools/transit_data_import.sh
+++ tools/transit_data_import.sh
@@ -15,29 +15,22 @@ else
 fi
 
 # download the data
-#mkdir $DATA
-#cd data && \
-#curl -O https://dl.dropbox.com/s/cwmcai0bwxg95mz/google_transit.zip && \
-#unzip google_transit.zip && \
-#rm google_transit.zip && \
-#cd ..
+mkdir $DATA
+cd data && \
+curl -O https://dl.dropbox.com/s/cwmcai0bwxg95mz/google_transit.zip && \
+unzip google_transit.zip && \
+rm google_transit.zip && \
+cd ..
 
 # install pymongo
 sudo easy_install pymongo
 
-
 # Imports transit data into MongoDB.  Default mongo host is localhost
 # add -h *host* for most commands
-echo "python import_stops.py      -p $PASS -h $HOST -f $DATA/stops.txt"
 python import_stops.py      -p $PASS -h $HOST -f $DATA/stops.txt
-echo "python import_routes.py     -p $PASS -h $HOST -f $DATA/routes.txt"
 python import_routes.py     -p $PASS -h $HOST -f $DATA/routes.txt
-echo "python import_stop_times.py -p $PASS -h $HOST -f $DATA/stop_times.txt"
 python import_stop_times.py -p $PASS -h $HOST -f $DATA/stop_times.txt
-echo "python import_trips.py      -p $PASS -h $HOST -f $DATA/trips.txt"
 python import_trips.py      -p $PASS -h $HOST -f $DATA/trips.txt
-echo "python lines_to_stops.py    ../data/routes.txt $DATA/trips.txt $DATA/stop_times.txt $HOST $PASS"
-python lines_to_stops.py    ../data/routes.txt $DATA/trips.txt $DATA/stop_times.txt $HOST $PASS
-echo "python push_next_stops.py   ../data/routes.txt $DATA/trips.txt $DATA/stop_times.txt $HOST $PASS"
-python push_next_stops.py   ../data/routes.txt $DATA/trips.txt $DATA/stop_times.txt $HOST $PASS
+python lines_to_stops.py    $DATA/routes.txt $DATA/trips.txt $DATA/stop_times.txt $HOST $PASS
+python push_next_stops.py   $DATA/routes.txt $DATA/trips.txt $DATA/stop_times.txt $HOST $PASS
 
